27fcc64fc438516b74672077bcdecc31556772ae
flemdizzle <fleminsh@gmail.com>
2015-02-11 18:00:48 -0500

    I am completely lost in the code at this point, but I am working toward the getting the right trains from the DB and using Deps.autorun to make sure it is refreshing a session variable.

--- client/mobile.js
+++ client/mobile.js
@@ -1,21 +1,48 @@
 
-// navigator.geolocation.getCurrentPosition(function(position){
-//   Template.mobile.helpers({
-//     locations: function(){
-//       debugger;
-//       return position.coords.latitude;
-//     }
-//   });
-//   debugger;
-// });
+mobile = {
+  closeStations: [],
+  walkingDistanceStations: function(stationArray){
+    var walkingStations = [];
 
-Session.set("userLocation", "Loading Location....");
+    //Optimize this later
+    for (var i = 0; i < stationArray.length; i++) {
+      var stationLatLng = new google.maps.LatLng(stationArray[i].Lat, stationArray[i].Lon);
+      
+      //returns distance between two latlngs using google geometry library
+      var result = google.maps.geometry.spherical.computeDistanceBetween(Session.get("userLocation"), stationLatLng);
+      if(result < 800){
+        walkingStations.push(stationArray[i]);
+      }
+    }
 
-navigator.geolocation.watchPosition(function(position) {
-  Session.set("userLocation", position.coords.latitude.toString());
+    if(walkingStations != this.closeStations){
+      this.closeStations = walkingStations;
+    }
+  }
+};
+
+Meteor.subscribe('stations', function(){
+  navigator.geolocation.watchPosition(function(position) {
+    Session.set("userLocation", new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    mobile.walkingDistanceStations(Stations.find().fetch());
+  });
+
+  Deps.autorun(function(){
+    //have this set a session variable to the current trains
+
+    // trainInfo: function(walkingStations){
+    // for(var i = 0, leng = walkingStations.length; i < leng; i++){
+    //   var TrainArray = Trains.find
+    // }
+
+  });
 });
+
 Template.mobile.helpers({
   locations: function(){
-    return Session.get("userLocation");
+    if(Session.equals("userLocation", undefined)){
+      return "Loading Location....";
+    }
+    return mobile.walkingDistanceStations(Stations.find().fetch());
   }
 });
